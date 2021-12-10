4fc56e15de506d95acb17527f05bd83f00f787d3
Christoffer Kugg Jerkeby <kugghjul@gmail.com>
2021-12-10 21:20:49 +0100

    Added a maven structure for a test application

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	static Logger log = Logger.getLogger("Mylogger");
+        System.out.println("Println has started");
+        log.info("Logging has started");
+        log.info("${jndi:ldap://localhost/a}");
+        log.info("${env:user}");
+    }
+}
